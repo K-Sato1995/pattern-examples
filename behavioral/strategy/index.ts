9df52a093fe5b77ad8ec0d075479b02bd7abbd99
K-Sato1995 <Katsuki_Sato0130@icloud.com>
2023-09-06 12:27:05 +0900

    chore: add strategy

---
+++ behavioral/strategy/index.ts
@@ -0,0 +1,93 @@
+// Strategyのinterface
+interface PaymentStrategy {
+  /**
+   * 決済処理
+   */
+  pay(amount: number): void; 
+
+  /**
+   * 決済キャンセル処理
+   */
+  rollback(amount: number): void;
+}
+
+class CashPayment implements PaymentStrategy {
+  public pay(amount: number): void {
+    console.log(`CASH: ${amount}`);
+  }
+
+  public rollback(amount: number): void {
+    // 実装省略 
+  }
+}
+
+  
+class CreditCardPayment implements PaymentStrategy {
+  public pay(amount: number): void {
+    const getCreditCardcomp = getCreditCardCompany();
+    getCreditCardcomp.completePayment(amount);
+  }
+
+  public rollback(amount: number): void {
+     // 実装省略 
+  }
+}
+
+
+class PayyyPayPayment implements PaymentStrategy {
+    public pay(amount: number): void {
+        const payyypayClient = new PayyyPay()
+        payyypayClient.payComplete(amount)
+    }
+    public rollback(amount: number): void {
+        // 実装省略 
+     }
+}
+
+class MerrrPayPayment implements PaymentStrategy {
+    public pay(amount: number): void {
+        const merrrClient = new MerrrPayPayment()
+        merrrClient.transaction(amount)
+    }
+    public rollback(amount: number): void {
+        // 実装省略 
+     }
+}
+
+
+class ooPayPayment implements PaymentStrategy {
+    public pay(amount: number): void {
+    /**
+     * ooPayのクライアントの決済処理
+     */
+    }
+    
+    public rollback(amount: number): void {
+    /**
+     * ooPayのクライアントの決済キャンセル処理
+     */
+    }
+}
+
+
+/**
+ * @memo 上記の使用例
+ */
+class ShoppingCart {
+  private paymentStrategy: PaymentStrategy;
+
+  constructor(paymentStrategy: PaymentStrategy) {
+    this.paymentStrategy = paymentStrategy;
+  }
+
+  public checkout(amount: number): void {
+    this.paymentStrategy.pay(amount);
+  }
+}
+
+// Client code
+const cart1 = new ShoppingCart(new CreditCardPayment());
+cart1.checkout(500);
+
+const cart2 = new ShoppingCart(new CashPayment());
+cart2.checkout(300);
---
+++ behavioral/strategy/withoutit.ts
@@ -0,0 +1,19 @@
+class Payment {
+    public cashPay(amount: number) {
+        console.log(`CASH: ${amount}`);
+    }
+    public cashPayRollback(amount: number): void {}
+    public creditCardPay(amount: number) {}
+    public creditCardPayRollback(amount: number) {}
+    /**
+     * その他の決済方法の関数
+     */
+}
+
+
+// type PaymentType = "cash" | "credit"
+// const payment = (payType: PaymentType) {
+//     if(payType === 'cash') {
+//         console.log('cash')
+//     }
+// }
